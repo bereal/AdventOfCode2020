7539672e4c00d4b6c9d2024d3fefe8f15d3a145b
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2020-12-06 23:50:48 +0200

    Nim: further improve separation of concerns

--- day06/solve.nim
+++ day06/solve.nim
@@ -2,38 +2,49 @@ import sequtils
 import strformat
 
 
-type Group = tuple
+type GroupCount = tuple
     all: int
     common: int
 
-proc `+`(g1: Group, g2: Group): Group =
+proc `+`(g1: GroupCount, g2: GroupCount): GroupCount =
     (g1.all + g2.all, g1.common + g2.common)
 
 
-const letters = {'a'..'z'}
+type Group = tuple
+    all: set[char]
+    common: set[char]
 
-iterator readGroups: Group =
+proc `*`(g1: Group, g2: Group): Group =
+    (g1.all + g2.all, g1.common * g2.common)
+
+proc parseLine(line: string): Group =
+    var s: set[char]
+    for c in line:
+        incl(s, c)
+    return (s, s)
+
+
+const emptyGroup: Group = ({}, {'a'..'z'})
+
+iterator readGroups: GroupCount =
     var line: string
-    var common = letters
-    var all: set[char] = {}
+    var groups: seq[Group] = @[]
+
+    proc summarize(): GroupCount =
+        let g = foldl(groups, a * b, emptyGroup)
+        return (len(g.all), len(g.common))
 
     while readLine(stdin, line):
         if len(line) == 0:
-            yield (len(all), len(common))
-            common = letters
-            all = {}
+            yield summarize()
+            setLen(groups, 0)
             continue
 
-        var cur: set[char]
-        for c in line:
-            incl(cur, c)
+        groups.add(parseLine(line))
 
-        common = common * cur
-        all = all + cur
-
-    if len(all) > 0:
-        yield (len(all), len(common))
+    if len(groups) > 0:
+        yield summarize()
 
 
-var g: Group = foldl(toSeq(readGroups), a + b)
+var g: GroupCount = foldl(toSeq(readGroups), a + b)
 echo fmt"{g.all} {g.common}"
