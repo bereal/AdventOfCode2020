ee54e8abca59d6daa1c7b3825ddad9c3725a89b0
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2020-12-22 19:10:52 +0200

    Scala: restructure code for part 2

--- 20_scala/solve.scala
+++ 20_scala/solve.scala
@@ -1,40 +1,70 @@
 import scala.io.StdIn
+import scala.collection.mutable.{Map => MMap}
 
-class Tile(val id: Int, val s: Array[String]) {
-  val init = s.map(_.toArray)
-  val allConfigs = Array(
-    init,
-    init.reverse,
-    init.map(_.reverse),
-    init.transpose,
-    init.transpose.reverse,
-    init.reverse.transpose,
-    init.transpose.reverse.map(_.reverse),
-    init.reverse.map(_.reverse)
+object D4 extends Enumeration {
+  val R0, R1, R2, R3, SH, SV, D1, D2 = Value
+
+  def apply(op: Value, square: Array[Array[Char]]): Array[Array[Char]] = {
+    op match {
+      case R0 => square
+      case SH => square.reverse
+      case SV => square.map(_.reverse)
+      case D1 => square.transpose
+      case D2 => square.reverse.map(_.reverse).transpose
+      case R1 => square.transpose.reverse
+      case R2 => square.reverse.map(_.reverse)
+      case R3 => square.reverse.transpose
+    }
+  }
+}
+
+object Side extends Enumeration {
+  val Top, Right, Bottom, Left = Value
+}
+
+class TileConfig(val id: Int, val d4: D4.Value, init: Array[Array[Char]]) {
+  import Side._
+  val body = D4(d4, init)
+  val perimeter = Map(
+    Top -> body(0).mkString,
+    Right -> body.map(_.last).mkString,
+    Bottom -> body.last.mkString,
+    Left -> body.map(_(0)).mkString
   )
 
-  val initSides =
-    Array(init(0), init.last, init.map(_(0)), init.map(_.last)).map(_.mkString)
-  val sides = allConfigs.map(_(0).mkString)
+  val lookup = perimeter.toSeq.map({ case (k -> v) => (v -> k) }).toMap
+}
+
+class Tile(val id: Int, val s: Array[String]) {
+  import Side._
+  type Row = Array[Char]
+
+  val init = s.map(_.toArray)
+  val allConfigs =
+    D4.values.toArray
+      .map(new TileConfig(id, _, init))
+      .groupBy(_.d4)
+      .view
+      .mapValues(_(0))
+      .toMap
 }
 
 class Area(val tiles: Seq[Tile]) {
-  val sideLookup =
-    tiles
-      .flatMap(c => c.sides.map({ _ -> c.id }))
-      .groupBy(_._1)
-      .view
-      .mapValues(_.map(_._2).toSet)
-      .toMap
+  import Side._
 
-  def findMatch(t: Tile) = t.initSides
-    .flatMap(
-      sideLookup.getOrElse(_, List.empty)
-    )
+  val sideLookup = tiles
+    .flatMap(c => c.allConfigs.values.map({ _.perimeter(Top) -> c.id }))
+    .groupBy(_._1)
+    .view
+    .mapValues(_.map(_._2).toSet)
+    .toMap
+
+  def findCorners() = sideLookup.toSeq
+    .filter(_._2.size == 1)
+    .groupBy(_._2.head)
+    .filter(_._2.length == 4) // two unique sides, each is mentioned twice
+    .keys
     .toSet
-    .filter(_ != t.id)
-
-  def findCorners(): Seq[Tile] = tiles.filter(findMatch(_).size <= 2)
 }
 
 object Main {
@@ -65,6 +95,6 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val area = new Area(readInput())
-    println(area.findCorners().map(_.id.toLong).reduce(_ * _))
+    println(area.findCorners().map(_.toLong).reduce(_ * _))
   }
 }
